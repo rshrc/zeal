0f84a120f3ac5a49d4b6c46a471eaea34a14b580
rshrc <rishieric91@gmail.com>
2019-02-03 16:56:18 +0530

    Added the notification page

--- lib/ui/pages/notification_page.dart
+++ lib/ui/pages/notification_page.dart
@@ -8,9 +8,25 @@ class NotificationPage extends StatefulWidget {
 class _NotificationPageState extends State<NotificationPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text("Notification Page"),
+    return ListView.builder(
+      itemCount: 20,
+      itemBuilder: (context, index) {
+        return getNotificationTile(index);
+      },
+    );
+  }
+
+  Widget getNotificationTile(int number) {
+    return ListTile(
+      leading: Icon(
+        Icons.message,
+        color: Colors.pinkAccent,
+      ),
+      title: Text("Notification $number"),
+      subtitle: Text("notification message"),
+      trailing: Icon(
+        Icons.remove,
+        color: Colors.blueGrey,
       ),
     );
   }
