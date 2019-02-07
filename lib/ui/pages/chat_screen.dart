743fd0df559b6a37c1c63e1be8bbadf353e18a7d
rshrc <rishieric91@gmail.com>
2019-02-07 18:57:33 +0530

    Added the whole Chat UI

--- lib/ui/pages/chat_screen.dart
+++ lib/ui/pages/chat_screen.dart
@@ -1,31 +1,77 @@
 import 'package:flutter/material.dart';
+import 'chat_message.dart';
 
 class ChatScreen extends StatefulWidget {
-  final String uid, username;
-
-  ChatScreen({this.uid, this.username});
-
   @override
-  _ChatScreenState createState() => _ChatScreenState();
+  State createState() => ChatScreenState();
 }
 
-class _ChatScreenState extends State<ChatScreen> {
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = TextEditingController();
+  final List<ChatMessage> _messages = <ChatMessage>[];
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    ChatMessage message = ChatMessage(
+      text: text,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+  Widget _textComposerWidget() {
+    return IconTheme(
+      data: IconThemeData(color: Colors.blue),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                decoration:
+                    InputDecoration.collapsed(hintText: "Send a message"),
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Friend Name"),
-      ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Flexible(
-              child: ListView.builder(
-                itemBuilder: (context, index) {},
-              ),
+      appBar: AppBar(title: Text("Friend Name"), centerTitle: true,),
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            child: ListView.builder(
+              padding: EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, int index) => _messages[index],
+              itemCount: _messages.length,
             ),
-          ],
-        ),
+          ),
+          Divider(
+            height: 1.0,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).cardColor,
+            ),
+            child: _textComposerWidget(),
+          ),
+        ],
       ),
     );
   }
