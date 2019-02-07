0d85a7abb785f95f3e458861b35001e934e42898
rshrc <rishieric91@gmail.com>
2019-02-07 19:21:37 +0530

    testing chat ui

--- lib/ui/pages/chat_message.dart
+++ lib/ui/pages/chat_message.dart
@@ -9,7 +9,45 @@ class ChatMessage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        _getFriendMessage(context),
+        _getUserMessage(context)
+      ],
+    );
+  }
+
+  Widget _getFriendMessage(BuildContext context) {
     return Container(
+      color: Colors.white30,
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(
+              child: Text(_friendName[0]),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(_friendName, style: Theme.of(context).textTheme.subhead),
+              Container(
+                margin: EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _getUserMessage(BuildContext context) {
+    return Container(
+      color: Colors.white30,
       margin: EdgeInsets.symmetric(vertical: 10.0),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
